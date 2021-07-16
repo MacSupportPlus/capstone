de18b6e02a738a34c704390dcfc689278e86527a
Andrena Alexandre <andrenaalexandre@gmail.com>
2021-07-16 16:13:46 -0400

    landing page video

--- client/src/Component/LandingPage.js
+++ client/src/Component/LandingPage.js
@@ -1,25 +1,17 @@
 import React from 'react';
-
+import YoutubeBackground from 'react-youtube-background';
 import '../index.css';
+
 
 export const LandingPage = () => {
     return(
         <>
-        <div className="mainHolder">
-            <h1><img src="../images/logo2.jpg"></img></h1>
-            <div className="videoHolder">
-                <div>
-                    this is one video
-                </div>
-                <div>
-                    this is another video
-                </div>
-                <div>
-                    finally a third video WAAAAAAA
-                </div>
-            </div>
-
-        </div>
+            <YoutubeBackground className="video"
+                videoId={'jycn4RqY_3U'}>
+                        <h1>Welcome</h1>
+            </YoutubeBackground>
+            <img src="../images/banner.jpg"></img>
+            <div>this is where content will go</div>
         </>
     )
 };
--- client/src/Component/Navbar.js
+++ client/src/Component/Navbar.js
@@ -14,6 +14,7 @@ import AccountCircle from '@material-ui/icons/AccountCircle';
 import MailIcon from '@material-ui/icons/Mail';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import MoreIcon from '@material-ui/icons/MoreVert';
+import '../index.css'
 
 const useStyles = makeStyles((theme) => ({
   grow: {
@@ -162,7 +163,7 @@ export default function PrimarySearchAppBar() {
   );
 
   return (
-    <div className>
+    <div className="">
       <div className={classes.grow}>
         <AppBar position="static" style={{backgroundColor: '#D4E09B'}}>
           <Toolbar>
