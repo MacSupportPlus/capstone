0f525a500a0008673a507fdf608bd31e393d4701
Q <quinta750@gmail.com>
2021-07-16 10:24:51 -0400

    pulled api for courses page and displayed it on the page

--- client/src/Component/Courses.js
+++ client/src/Component/Courses.js
@@ -1,5 +1,7 @@
 import React, {useEffect,useState} from 'react';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Link} from 'react-router-dom';
 
 export const Courses = () =>{
         const[coursesData,setCoursesData]= useState([])
@@ -8,19 +10,23 @@ export const Courses = () =>{
             .then(res => setCoursesData(res.data.result.results))
         },[])
         
-
         return(
             <>
             <h1>Course Page</h1>
-            {coursesData && coursesData.map((course) => (
-            <div>
-            <img src={course.image_240x135}></img>
-            <p>{course.title} <br /> {course.price}
-            </p>
-
+            <div className="row" style={{fontSize:"15px"}}>
+                <div className="d-flex col-12 column-flex flex-wrap p-4">
+                    {coursesData && coursesData.map((course) => (
+                        <div className="col-3">
+                            <Link to={`udemy.com/${course.url}`}>
+                            <img src={course.image_240x135} style={{width:"400px"}}></img>
+                            </Link>
+                            <div className="col-10" style={{textAlign:"center"}}>
+                                <p>{course.title} <br /> {course.price} <br /></p>
+                            </div>
+                        </div>
+                ))}
+                </div>
             </div>
-            ))}
             </>
         )
-
 };
