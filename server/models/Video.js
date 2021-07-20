f018bf45f180895f47fea88b4280b2db715ea6d7
Jason McElvain <jasonom2@yahoo.com>
2021-07-20 13:18:36 -0500

    get all videos route from db update

--- server/models/Video.js
+++ server/models/Video.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 
 const VideoSchema = new mongoose.Schema({
-    userId: {
-        type:String,
-        require: true,
-    },
+    // userId: {
+    //     type:String,
+       
+    // },
     description : {
         type:String,
         max: 500
